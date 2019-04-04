26babb30e482233adb52faec6f212376787ad986
大师行 <evan199101@gmail.com>
2019-04-04 18:17:45 +0800

    设置路由

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
+// 引入路由
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// 测试路由
+function Home() {
+    return <h2>Home</h2>;
+}
+
+// 测试路由
+function About() {
+    return <h2>About</h2>;
+}
+
 class App extends Component {
     render() {
-        return <div className="App">hello world</div>;
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                </div>
+            </Router>
+        );
     }
 }
 
