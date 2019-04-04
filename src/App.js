8cdeeb3c48299d31bd5ba364d7d648dc05eebd1b
大师行 <evan199101@gmail.com>
2019-04-04 18:45:43 +0800

    添加模块 styled-components   设置reset.css全局样式

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import React, { Component } from "react";
 // 引入路由
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+// 引入全局样式
+import GlobalStyle from "./reset";
+
 // 测试路由
 function Home() {
     return <h2>Home</h2>;
 }
 
-// 测试路由
-function About() {
-    return <h2>About</h2>;
-}
-
+// 设置APP根目录
 class App extends Component {
     render() {
         return (
             <Router>
                 <div>
+                    <GlobalStyle />
                     <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
                 </div>
             </Router>
         );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// 引入react
 import React from "react";
 import ReactDOM from "react-dom";
+
+// 引入APP组件
 import App from "./App";
+
+// 引入缓存提升速度
 import * as serviceWorker from "./serviceWorker";
 
 
